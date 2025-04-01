e8df56e6797d00d2fb3c09929bba5c27a7d96d0b
DmitryDol <ddimapalih@gmail.com>
2025-04-02 03:51:19 +0400

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
-import asyncio
+import uvicorn
+from fastapi import FastAPI
+
+from src.api.main import api_router
 import os
 import sys
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from src.queries.orm import AsyncORM
 
 
-async def main():
-    # await AsyncORM.create_tables()
-    await AsyncORM.drop_tables()
+app = FastAPI(
+    title="Сервис для трекинга карьеры"
+)
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvicorn.run(app="main:app", reload=True)
