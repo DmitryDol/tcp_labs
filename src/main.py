4c9873e628ddfc243efd2061ae66e5f7207ee78e
Alenafaust <alenafaust24@gmail.com>
2025-04-02 18:20:43 +0400

    Remove conflicting async main function and set up FastAPI application

--- src/main.py
+++ src/main.py
@@ -8,17 +8,11 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 
-
-<<<<<<< HEAD
-async def main():
-    await AsyncORM.create_tables()
-=======
 app = FastAPI(
     title="Сервис для трекинга карьеры"
 )
 
 app.include_router(api_router)
->>>>>>> 6071a92abb44c8e658552940319d4c3aff800f45
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
