560fd71992f487f17c2d0c6ab9228cbd16038261
DmitryDol <ddimapalih@gmail.com>
2025-04-03 19:33:27 +0400

    add connection to roadmap api

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from api.routes import users
+from api.routes import roadmaps, users
 from config import settings
 
 api_router = APIRouter()
 api_router.include_router(users.router)
+api_router.include_router(roadmaps.router)
