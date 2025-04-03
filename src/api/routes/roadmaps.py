427e51574cc039dcd18301b49f948ff51ce594d8
DmitryDol <ddimapalih@gmail.com>
2025-04-03 19:32:34 +0400

    add GET, POST, PATCH, DELETE routes for roadmaps update corresponding methods in the RoadmapsService

--- src/api/routes/roadmaps.py
+++ src/api/routes/roadmaps.py
@@ -1,5 +1,5 @@
-from typing import Annotated, Optional
-from fastapi import APIRouter, Depends, FastAPI, HTTPException, Path
+from typing import Annotated
+from fastapi import APIRouter, Path
 from dto import RoadmapAddDTO, RoadmapEditDTO
 from api.dependencies import UOWDep
 from services.roadmaps import RoadmapsService
@@ -20,3 +20,34 @@ async def get_public_roadmaps(
     roadmaps = await RoadmapsService.get_public_roadmaps(uow, search, difficulty, limit)
     return roadmaps
     
+@router.post("")
+async def add_roadmap(
+    roadmap: RoadmapAddDTO,
+    uow: UOWDep
+):
+    roadmap_id, user_id = await RoadmapsService.add_roadmap(uow, roadmap)
+    return {"roadmap_id": roadmap_id, "user_id": user_id}
+
+@router.get("/{roadmap_id}")
+async def get_roadmap_info(
+    roadmap_id: Annotated[int, Path(title="Roadmap id")],
+    uow: UOWDep
+):
+    extended_roadmap = await RoadmapsService.get_roadmap_extended(uow, roadmap_id)
+    return extended_roadmap
+
+@router.patch("/{roadmap_id}")
+async def edit_roadmap(
+    roadmap_id: Annotated[int, Path(title="Roadmap id")],
+    roadmap: RoadmapEditDTO,
+    uow: UOWDep
+):
+    await RoadmapsService.edit_roadmap(uow, roadmap_id, roadmap)
+    return {"roadmap_id": roadmap_id}
+
+@router.delete("/{roadmap_id}")
+async def delete_roadmap(
+    roadmap_id: Annotated[int, Path(title="Roadmap id")],
+    uow: UOWDep
+):
+    await RoadmapsService.delete_roadmap(uow, roadmap_id)
