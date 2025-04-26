e5fc8c49678684c7c60e40f1de1e807625ea272f
DmitryDol <ddimapalih@gmail.com>
2025-04-27 03:43:51 +0400

    Add an @asynccontextmanager for init redis_client and middleware for measuring request processing time. Improved logging.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,51 @@
-from datetime import datetime
+from contextlib import asynccontextmanager
+import logging
+import time
 from typing import Callable
+import redis.asyncio as redis
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.main import api_router
 import os
 import sys
 
+from config import configure_logging, settings
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
+configure_logging(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.debug('Initializing Redis client.')
+    redis_client = redis.Redis(
+        host=settings.REDIS_HOST, 
+        port=settings.REDIS_PORT, 
+        db=settings.REDIS_DB, 
+        decode_responses=True
+    )
+    try:
+        await redis_client.ping()
+        logger.debug("Redis connection successful.")
+        app.state.redis_client = redis_client
+        yield
+    finally:
+        logger.debug("Closing Redis connection.")
+        await app.state.redis_client.close()
+        logger.debug("Redis connection closed.")
+
+    # email_devops() to notify that server is down
 
 app = FastAPI(
-    title="Сервис для трекинга карьеры"
+    title="Сервис для трекинга карьеры",
+    lifespan=lifespan
 )
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,6 +56,17 @@ app.add_middleware(
 )
 
 
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next: Callable):
+    start_time = time.monotonic()
+    response = await call_next(request)
+    process_time = time.monotonic() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info(f"Request {request.method} {request.url.path} processed in {process_time:.4f} seconds") 
+    return response
+
+
+    
 
 app.include_router(api_router)
 
