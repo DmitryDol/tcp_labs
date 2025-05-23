ea404338cbd29e81ac46a0b503ae5207ada1da6b
DmitryDol <ddimapalih@gmail.com>
2025-05-23 13:44:26 +0400

    update vite config for containerization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "127.0.0.1",
+    host: true, //"127.0.0.1",
     port: 5050,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
-      '/api/core': {
-        target: 'http://localhost:8000',
+      "/api/core": {
+        target: "http://fastapi_app:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/core/, '')
+        rewrite: (path) => path.replace(/^\/api\/core/, ""),
       },
-      '/api/images': {
-        target: 'http://localhost:8080',
+      "/api/images": {
+        target: "http://localhost:8080",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/images/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api\/images/, ""),
+      },
+    },
   },
-})
+});
